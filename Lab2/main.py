2ea2252a4033c0c274725813212ba30e72bf7494
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2024-12-13 13:46:08 +0530

    Added tokens frequency

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,4 +1,5 @@
 import csv
+import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 
 read_file = open('somefile.txt', 'r')
@@ -6,10 +7,12 @@ lines = read_file.readlines()
 
 n = len(lines)
 
-rows = [["NewsBody", "NewsHeadline", "label", "WordTokensLength", 'SentTokensLength']]
+rows = [["NewsBody", "NewsHeadline", "label", "WordTokensLength", 'SentTokensLength', 'TokensFrequency']]
 
 for i in range(0,n,3):
-    row = [lines[i], lines[i + 1], lines[i + 2], len(word_tokenize(lines[i])), len(sent_tokenize(lines[i]))]
+    tokens = word_tokenize(lines[i])
+    tokens_freq = nltk.FreqDist(tokens)
+    row = [lines[i], lines[i + 1], lines[i + 2], len(tokens), len(sent_tokenize(lines[i])), tokens_freq.items()]
     rows.append(row)
 
 with open("output.tsv", 'w', newline='') as csvfile:
